e615a2d8e0bc5e2b18b2030772fccc58ee353212
Aman Giri <aman.giri0104@gmail.com>
2025-09-04 15:29:42 +0530

    angular@18 initial code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { CustomerComponent } from './src/components/customer/customer.component';
-import { CustomerCopyComponent } from './src/components/customer-copy/customer-copy.component';
 
-export const routes: Routes = [
-    {path:'', component:CustomerComponent},
-    {path:'copy', component: CustomerCopyComponent}
-];
+export const routes: Routes = [];
